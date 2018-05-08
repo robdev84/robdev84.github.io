ce4e153e00bf3b5f2635eb0ec7e75af4546e08be
Robert <robertn84@gmail.com>
2018-05-08 18:33:06 -0500

    Update

--- js/buttons.js
+++ js/buttons.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {   
     $("button").click(
         function() {
-            var fileName = "../json/" + $(this).attr("title") + ".json"; //../ takes you out of the folder
+            var fileName = "https://robdev84.github.io/json/" + $(this).attr("title") + ".json"; //../ takes you out of the folder
             $.getJSON(fileName, function(data) {
                 $.each(data, function() {
                     $.each(this, function(key, value) {
                         $("main").text("");
                         $("main").append(
                             "<h2>" + value.dates + "</h2>" +
-                            "<h2>" + value.street + "</h2>" 
+                            "<h2>" + value.street + "</h2>"
                         );
                     });
                 });
